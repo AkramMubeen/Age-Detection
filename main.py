d671986ddaaead4a1c2ffcab6f3899cf7cafffda
AkramMubeen <amubeen457@gmail.com>
2023-11-21 04:45:40 +0500

    Done With Stage 2/3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from AgeDetection import logger
 from AgeDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from AgeDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -10,6 +10,28 @@ try:
    logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Prepare Base Model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
